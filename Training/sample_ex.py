1cb8beb5445beeafb02364ec3b89f0c089a0e9b8
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-04 14:54:50 +0900

    Fix samples

--- Training/sample_ex.py
+++ Training/sample_ex.py
@@ -1,66 +1,66 @@
 while True:
-    num = input('문제 번호 입력 (문제 번호 외 입력시 종료)> ')
-    if num == '1':
+    example_number = input('문제 번호 입력 (문제 번호 외 입력시 종료)> ')
+    if example_number == '1':
         print('# 1')
         ########## training code
         ##########
         print()
         
-    elif num == '2':
+    elif example_number == '2':
         print('# 2')
         ########## training code
         ##########
         print()
         
-    elif num == '3':
+    elif example_number == '3':
         print('# 3')
         ########## training code      
         ##########
         print()
         
-    elif num == '4':
+    elif example_number == '4':
         print('# 4')
         ########## training code
         ##########
         print()
         
-    elif num == '5':
+    elif example_number == '5':
         print('# 5')
         ########## training code
         ##########
         print()
         
-    elif num == '6':
+    elif example_number == '6':
         print('# 6')
         ########## training code
         ##########
         print()
         
-    elif num == '7':
+    elif example_number == '7':
         print('# 7')
         ########## training code
         ##########
         print()
         
-    elif num == '8':
+    elif example_number == '8':
         print('# 8')
         ########## training code
         ##########
         print()
         
-    elif num == '9':
+    elif example_number == '9':
         print('# 9')
         ########## training code
         ##########
         print()
         
-    elif num == '10':
+    elif example_number == '10':
         print('# 10')
         ########## training code
         ##########
         print()
         
-    elif num == '11':
+    elif example_number == '11':
         print('# 11')
         ########## training code
         ##########
--- Training/sample_tr.py
+++ Training/sample_tr.py
@@ -1,70 +1,70 @@
 while True:
-    num = input('문제 번호 입력 > ')
-    if num == '1':
+    problem_number = input('문제 번호 입력 > ')
+    if problem_number == '1':
         print('# 1. ')
-        ##### solution code
-        num1 = int(input('정수를 입력하세요 > '))
+        ########## solution code
+        num = int(input('정수를 입력하세요 > '))
         
-        #####
+        ##########
         print()
         
-    elif num == '2':
+    elif problem_number == '2':
         print('# 2. ')
         ########## solution code
         
         ##########
         print()
         
-    elif num == '3':
+    elif problem_number == '3':
         print('# 3. ')
         ########## solution code
         
         ##########
         print()
        
-    elif num == '4':
+    elif problem_number == '4':
         print('# 4. ')
         ########## solution code
         
         ##########
         print()
         
-    elif num == '5':
+    elif problem_number == '5':
         print('# 5. ')
         ########## solution code
         
         ##########
         print()
         
-    elif num == '6':
+    elif problem_number == '6':
         print('# 6. ')
         ########## solution code
         
         ##########
         print()
        
-    elif num == '7':
+    elif problem_number == '7':
         print('# 7. ')
         ########## solution code
         
         ##########
         print()
         
-    elif num == '8':
+    elif problem_number == '8':
         print('# 8. ')
         ########## solution code
         
         ##########
         print()
        
-    elif num == '9':
+    elif problem_number == '9':
         print('# 9. ')
         ########## solution code
         
         ##########
         print()
        
-    elif num == '10':
+    elif problem_number == '10':
         print('# 10. ')
         ########## solution code
         
