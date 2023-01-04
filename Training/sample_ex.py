c8234ed32d83d3ed3bf22eb1336a422aabd4a714
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-01-04 14:38:37 +0900

    Fix sameple file

---
+++ Training/sample_ex.py
@@ -0,0 +1,71 @@
+while True:
+    num = input('문제 번호 입력 (문제 번호 외 입력시 종료)> ')
+    if num == '1':
+        print('# 1')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '2':
+        print('# 2')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '3':
+        print('# 3')
+        ########## training code      
+        ##########
+        print()
+        
+    elif num == '4':
+        print('# 4')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '5':
+        print('# 5')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '6':
+        print('# 6')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '7':
+        print('# 7')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '8':
+        print('# 8')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '9':
+        print('# 9')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '10':
+        print('# 10')
+        ########## training code
+        ##########
+        print()
+        
+    elif num == '11':
+        print('# 11')
+        ########## training code
+        ##########
+        print()
+        
+    else:
+        print('Terminated!')
+        break
--- Training/sample_tr.py
+++ Training/sample_tr.py
@@ -2,17 +2,75 @@ while True:
     num = input('문제 번호 입력 > ')
     if num == '1':
         print('# 1. ')
-        ### solution code
+        ##### solution code
         num1 = int(input('정수를 입력하세요 > '))
         
-        ###
+        #####
         print()
+        
     elif num == '2':
         print('# 2. ')
-        ### solution code
-        num2 = int(input('정수를 입력하세요 > '))
+        ########## solution code
         
-        ###
+        ##########
+        print()
+        
+    elif num == '3':
+        print('# 3. ')
+        ########## solution code
+        
+        ##########
+        print()
+       
+    elif num == '4':
+        print('# 4. ')
+        ########## solution code
+        
+        ##########
+        print()
+        
+    elif num == '5':
+        print('# 5. ')
+        ########## solution code
+        
+        ##########
+        print()
+        
+    elif num == '6':
+        print('# 6. ')
+        ########## solution code
+        
+        ##########
+        print()
+       
+    elif num == '7':
+        print('# 7. ')
+        ########## solution code
+        
+        ##########
+        print()
+        
+    elif num == '8':
+        print('# 8. ')
+        ########## solution code
+        
+        ##########
+        print()
+       
+    elif num == '9':
+        print('# 9. ')
+        ########## solution code
+        
+        ##########
+        print()
+       
+    elif num == '10':
+        print('# 10. ')
+        ########## solution code
+        
+        ##########
+        print()
+       
     else:
         print('Terminated!')
         break
